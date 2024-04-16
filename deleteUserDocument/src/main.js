30343433877ccf0c4e24954c9e5e2629af928b9b
Derrick L. Mayiku <derrickmal123@gmail.com>
2024-04-16 14:50:19 +0300

    Update main.js

--- deleteUserDocument/src/main.js
+++ deleteUserDocument/src/main.js
@@ -16,7 +16,7 @@ export default async ({ req, res, log, error }) => {
   // If something goes wrong, log an error
   error('Hello, Errors!');
 
-  // The `req` object contains the request data
+  // The `req` object contains the request data as needed
   if (req.method === 'GET') {
     // Send a response with the res object helpers
     // `res.send()` dispatches a string back to the client
